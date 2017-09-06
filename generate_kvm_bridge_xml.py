156d16970b8a9ea8fa447f1048b0510b3c175b8f
hknam <hknam0205@gmail.com>
2017-09-06 16:30:44 +0900

    kvm xml template add

---
+++ generate_kvm_bridge_xml.py
@@ -0,0 +1,27 @@
+
+from lxml import etree
+
+network = etree.Element('network')
+
+name = etree.Element('name')
+name.text = 'default'
+network.append(name)
+
+forward = etree.Element("forward", mode="nat")
+bridge = etree.Element("brigde", name="virbr0", stp="on", delay="0")
+mac = etree.Element("mac", address="52:54:00:ca:6a:d7")
+ip = etree.Element("ip", address = "192.168.122.1", netmask = "255.255.255.0")
+
+dhcp = etree.SubElement(ip, 'dhcp')
+range = etree.SubElement(dhcp, 'range', end='192.168.122.254', start='192.168.122.2')
+host = etree.SubElement(dhcp, 'host', mac='52:54:00:a0:cc:19', name='client01', ip='192.168.122.2')
+
+network.append(forward)
+network.append(bridge)
+network.append(mac)
+network.append(ip)
+
+xml = etree.tostring(network, pretty_print=True)
+
+tree = etree.ElementTree(network)
+tree.write("test.xml")
