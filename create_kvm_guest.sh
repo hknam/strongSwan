5363b32340fb37e7eca0ee11012a0d28d5edc069
hknam <hknam0205@gmail.com>
2017-09-18 22:50:53 +0900

    base image create

--- create_kvm_guest.sh
+++ create_kvm_guest.sh
@@ -12,3 +12,15 @@ echo -e "${GREEN}'Successful : Create virtual bridge '${NORMAL}"
 qemu_image_install="qemu-img create -f qcow2 ./baseimage.qcow 8G"
 $qemu_image_install
 echo -e "${GREEN}'Successful : Create qemu image '${NORMAL}"
+
+create_base_image="virt-install \
+                  --name base-image \
+                  --ram 1024 \
+                  --disk path=./baseimage.qcow2 \
+                  --os-type linux \
+                  --graphics none \
+                  --console pty,target_type=serial \
+                  --cdrom './ubuntu-16.04.3-server-amd64.iso' \
+                  --extra-args 'console=ttyS0,115200n8 serial'
+"
+$create_base_image
