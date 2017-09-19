22a0e02605ac26248cd1c18a2dbf854cce078f1f
hknam <hknam0205@gmail.com>
2017-09-19 11:10:14 +0900

    create kvm image

--- create_kvm_guest.sh
+++ create_kvm_guest.sh
@@ -5,14 +5,17 @@ GREEN=$(tput setaf 2)
 YELLOW=$(tput setaf 3)
 NORMAL=$(tput op)
 
-create_virtual_bridge="brctl addbr virbr0"
-$create_virtual_bridge
-echo -e "${GREEN}'Successful : Create virtual bridge '${NORMAL}"
+bridge_name='default'
+#create virtual bridge
+virsh net-define $bridge_name'.xml'
+vursg net-start $bridge_name
 
+#create qemu image
 qemu_image_install="qemu-img create -f qcow2 ./baseimage.qcow2 8G"
 $qemu_image_install
 echo -e "${GREEN}'Successful : Create qemu image '${NORMAL}"
 
+#create kvm-image from ubuntu archive
 virt-install \
   --name base-image \
   --ram 1024 \
@@ -23,3 +26,11 @@ virt-install \
   --console pty,target_type=serial \
   --location 'http://kr.archive.ubuntu.com/ubuntu/dists/xenial/main/installer-amd64' \
   --extra-args 'console=ttyS0,115200n8 serial'
+
+#shutdown base-image
+virsh shutdown base-image
+
+#mount guest's disk and enable a service
+guestmount -d base-image -i /mnt
+ln -s /mnt/lib/systemd/system/getty@.service /mnt/etc/systemd/system/getty.target.wants/getty@ttyS0.service
+umount /mnt
--- install-strongswan.sh
+++ install-strongswan.sh
@@ -11,33 +11,40 @@ wget_install="wget "
 uncompress_tar="tar xvf "
 move_folder='cd '
 
+#package update
 $package_update
 echo -e "${GREEN}'Successful : Update package sources'${NORMAL}"
 
+#install softwares
 install_package_list='wget make gcc libgmp3-dev build-essential bridge-utils qemu libvirt-bin libguestfs-tools virtinst'
 $package_install$install_package_list
 echo -e "${GREEN}'Successful : Install packages from custom repo '${NORMAL}"
 
+#strongSwan source code download
 strongswan_url='https://download.strongswan.org/strongswan-5.6.0.tar.bz2'
 $wget_install$strongswan_url
 echo -e "${GREEN}'Successful : Install strongSwan source file from '$strongswan_url${NORMAL}"
 
+#unpack the strongSwan source code
 strongswan_source_file=${strongswan_url:32}
 echo $strongswan_source_file
 $uncompress_tar$strongswan_source_file
 echo -e "${GREEN}'Successful : Unpack the tar file '${NORMAL}"
 
-
+#move strongSwan directory
 target_strongswan_dir=${strongswan_source_file:0:16}
 cd $target_strongswan_dir
 strongswan_dir=$PWD
 
+#configure
 configure=$strongswan_dir$'/configure --prefix=/usr --sysconfdir=/etc'
 $configure
 echo -e "${GREEN}'Successful : Configure strongSwan using options '${NORMAL}"
 
+#make
 make
 echo -e "${GREEN}'Successful : Build the strongSwan source '${NORMAL}"
 
+#install strongSwan
 make install
 echo -e "${GREEN}'Successful : Install the strongSwan '${NORMAL}"
