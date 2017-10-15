398d7d5e2cc2305ee714400489ec55f2d73e63af
hknam <hknam0205@gmail.com>
2017-10-15 23:20:08 +0900

    client certificate initial commit

--- python-script/generate_vpn_certificate.py
+++ python-script/generate_vpn_certificate.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 
 server_ip = '192.168.122.2'
 
@@ -23,6 +24,49 @@ def create_server_certificate():
 
     print_certificate = 'openssl x509 -inform DER -in /etc/ipsec.d/certs/vpnHostCert.der -noout -text'
     run_command(print_certificate)
+
+    with open('/etc/ipsec.secrets', 'a') as outfile:
+        outfile.write(': RSA vpnHostKey.der')
+
+    print_ipsec_certs = 'ipsec listcerts'
+    run_command(print_ipsec_certs)
+
+def get_client_list():
+    with open('./config/clients.csv', 'r') as f:
+        clients = f.read()
+
+    return clients
+
+def create_client_certificate():
+    #name, mac_addr, ip_addr
+    clients = get_client_list()
+
+    server_ip = clients[0].split(',')[2]
+
+    for client in clients[1:]:
+        name = client.split(',')[0]
+        ip_addr = client.split(',')[2]
+        client_config_dir = './config/' + name
+
+        if not os.path.exists(client_config_dir):
+            os.makedirs(client_config_dir)
+
+        output_filename = client_config_dir + '/' + name
+        ipsec_pki_gen = 'ipsec pki --gen --type rsa --size 2048 --outform der > ' + output_filename + 'Key.der'
+        run_command(ipsec_pki_gen)
+
+        chmod = 'chmod 600 ' + output_filename + 'Key.der'
+        run_command(chmod)
+
+        gen_public_key = 'ipsec pki --pub --in ' + output_filename + '.der' + ' --type rsa | ipsec pki --issue --lifetime 730 --cacert cacerts/strongswanCert.der --cakey private/strongswanKey.der --dn "C=NL, O=Example Company, CN=' + name + '@example.org" --san ' + ip_addr + ' --outform der > ' + output_filename + 'Cert.der'
+        run_command(gen_public_key)
+
+        private_pem = 'openssl rsa -inform DER -in ' + output_filename + '.der' + ' -out ' + output_filename + 'Key.pem -outform PEM'
+        run_command(private_pem)
+
+        cert_pem = 'openssl rsa -inform DER -in ' + output_filename + '.der' + ' -out ' + output_filename + 'Cert.pem -outform PEM'
+        run_command(cert_pem)
+
 
 def create_ipsec_conf(server_ip, client_ip, client_name):
     with open('ipsec.conf', 'w') as outfile:
